2fed11b42d05136675787229c39258c11631dc01
Florin Petre <fmihailpetre@gmail.com>
2015-01-24 09:55:12 -0500

    make a table of search results along with distributor info

--- autosuggest.js
+++ autosuggest.js
@@ -23,22 +23,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     AutoSuggest.parseResponse = function (search_response) {
       search_response.results.forEach(function(result, i){
+        var resultsTemplate = document.getElementById("results-template").innerHTML;
         var part = result.item;
         var brandName = part.brand.name;
         var partMpn = part.mpn;
         var partSnippet = result.snippet;
-        var resultsTemplate = document.getElementById("results-template").innerHTML;
+        var offers = part.offers;
+        
 
-
+        resultsTemplate = resultsTemplate.replace("{ {tblnum} }", i);
         resultsTemplate = resultsTemplate.replace("{ {brandName} }", brandName);
         resultsTemplate = resultsTemplate.replace("{ {partMpn} }", partMpn);
         resultsTemplate = resultsTemplate.replace("{ {partSnippet} }", partSnippet);
-
         resultsTable.innerHTML += resultsTemplate;
 
+        for (var j = 0; j < offers.length; j++) {
+          if (j > 3) {break};
+          var offer = offers[j];
+          var distributor = offer.seller.name;
+          var sku = offer.sku;
+          var stock = offer.in_stock_quantity;
+          var productUrl = offer.product_url;
+          var resultsPartInfoEl = document.getElementById("results-part-info-" + i);
+          var resultsPartInfoTemplate = document.getElementById("results-part-info-template").innerHTML;
+          var resultsPartInfoTr = document.createElement("tr");
 
+          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {distributor} }", distributor);
+          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {sku} }", sku);
+          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {stock} }", stock);
+          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {url} }", productUrl);
+          resultsPartInfoTr.innerHTML = resultsPartInfoTemplate;
 
+          resultsPartInfoEl.appendChild(resultsPartInfoTr);
 
+        };
       });
     };
 
