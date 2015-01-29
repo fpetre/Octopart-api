58a9140d1eb1a534138a34ad9ce064f153291248
Florin Petre <fmihailpetre@gmail.com>
2015-01-29 10:13:00 -0500

    fix bug with submit event

--- autosuggest.js
+++ autosuggest.js
@@ -11,8 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
   var searchInput = document.querySelector(".parts-search-suggestions-input > input");
   var searchForm = document.querySelector(".parts-search-form");
 
+
+
   //autosuggest
   searchInput.addEventListener("keyup", function (event){
+    if (event.keyIdentifier === "Enter" ) {return};
     var dataSrc = document.createElement('script');
     var searchQuery = event.target.value;
     var searchUrl = url + "&apikey=" + OCTOPART_API_KEY + "&q=" + searchQuery + "*";
@@ -25,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (searchQuery.length < 2 || search_response.results.length === 0) {break};
         var searchResultEl = document.createElement("li");
         var item = search_response.results[i].item;
+        searchResultEl.className = "parts-search-suggestion";
         searchResultEl.innerHTML = item.mpn;
         searchSuggestionsEl.appendChild(searchResultEl);
       };
@@ -33,83 +37,97 @@ document.addEventListener('DOMContentLoaded', function () {
       } else if (searchSuggestionsEl.childNodes.length == 0) {
         searchSuggestionsEl.className = "";
       }
+
+      //add click listener on search suggestions
+      var searchSuggestions = document.querySelectorAll(".parts-search-suggestion");
+      for (var i = 0; i < searchSuggestions.length; i++) {
+        searchSuggestions[i].addEventListener("click", submitAutoSuggest);
+      };
+
     };
+    document.querySelector(".test").appendChild(dataSrc);
+  });
+
+
+  //submit autosuggest
+  function submitAutoSuggest (event) {
+    var dataSrc = document.createElement('script');
+    var autoSuggestResult = event.target.innerHTML;
+    var autoSuggestResults = document.querySelector(".parts-search-suggestions > ul");
+    var searchUrl = url + "&apikey=" + OCTOPART_API_KEY + "&q=" + autoSuggestResult;
+    dataSrc.src = searchUrl;
+    searchInput.value = "";
+    autoSuggestResults.innerHTML = "";
+    autoSuggestResults.className = "";
+
+    AutoSuggest.parseResponse = submitSearch;
 
     document.querySelector(".test").appendChild(dataSrc);
-    // addBorders(); // adds correct borders
-
-  });
+  };
 
 
   //submit search
+  function submitSearch (search_response) {
+    var resultsTable = document.getElementById("results");
+    resultsTable.innerHTML = "";
+    search_response.results.forEach(function(result, i){
+      var resultsTemplate = document.getElementById("results-template").innerHTML;
+      var part = result.item;
+      var brandName = part.brand.name;
+      var partMpn = part.mpn;
+      var partSnippet = result.snippet;
+      var offers = part.offers;
+
+      resultsTemplate = resultsTemplate.replace("{ {tblnum} }", i);
+      resultsTemplate = resultsTemplate.replace("{ {brandName} }", brandName);
+      resultsTemplate = resultsTemplate.replace("{ {partMpn} }", partMpn);
+      resultsTemplate = resultsTemplate.replace("{ {partSnippet} }", partSnippet);
+      resultsTable.innerHTML += resultsTemplate;
+
+
+      var numOfOffers = Math.min(4, offers.length);
+      for (var j = 0; j < numOfOffers; j++) {
+        var offer = offers[j];
+        var distributor = offer.seller.name;
+        var sku = offer.sku;
+        var stock = offer.in_stock_quantity;
+        var productUrl = offer.product_url;
+        var resultsPartInfoEl = document.getElementById("results-part-info-" + i);
+        var resultsPartInfoTemplate = document.getElementById("results-part-info-template").innerHTML;
+        var resultsPartInfoTr = document.createElement("tr");
+
+        resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {distributor} }", distributor);
+        resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {sku} }", sku);
+        resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {stock} }", stock);
+        resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {url} }", productUrl);
+        resultsPartInfoTr.innerHTML = resultsPartInfoTemplate;
+        resultsPartInfoEl.appendChild(resultsPartInfoTr);
+
+        resultsTable.setAttribute("border", "1");
+        resultsTable.setAttribute("cellpadding", "5");
+        resultsTable.setAttribute("cellspacing", "5");
+
+      };
+    });
+
+
+  };
+
+
   searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    var resultsTable = document.getElementById("results");
     var dataSrc = document.createElement('script');
+    var autoSuggestResults = document.querySelector(".parts-search-suggestions > ul");
     var searchQuery = searchInput.value;
     var searchUrl = url + "&apikey=" + OCTOPART_API_KEY + "&q=" + searchQuery;
     searchInput.value = "";
-    resultsTable.innerHTML = "";
+    autoSuggestResults.innerHTML = "";
+    autoSuggestResults.className = "";
     dataSrc.src = searchUrl;
-
-    AutoSuggest.parseResponse = function (search_response) {
-      search_response.results.forEach(function(result, i){
-        var resultsTemplate = document.getElementById("results-template").innerHTML;
-        var part = result.item;
-        var brandName = part.brand.name;
-        var partMpn = part.mpn;
-        var partSnippet = result.snippet;
-        var offers = part.offers;
-        
-
-        resultsTemplate = resultsTemplate.replace("{ {tblnum} }", i);
-        resultsTemplate = resultsTemplate.replace("{ {brandName} }", brandName);
-        resultsTemplate = resultsTemplate.replace("{ {partMpn} }", partMpn);
-        resultsTemplate = resultsTemplate.replace("{ {partSnippet} }", partSnippet);
-        resultsTable.innerHTML += resultsTemplate;
-
-        for (var j = 0; j < 4; j++) {
-          var offer = offers[j];
-          var distributor = offer.seller.name;
-          var sku = offer.sku;
-          var stock = offer.in_stock_quantity;
-          var productUrl = offer.product_url;
-          var resultsPartInfoEl = document.getElementById("results-part-info-" + i);
-          var resultsPartInfoTemplate = document.getElementById("results-part-info-template").innerHTML;
-          var resultsPartInfoTr = document.createElement("tr");
-
-          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {distributor} }", distributor);
-          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {sku} }", sku);
-          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {stock} }", stock);
-          resultsPartInfoTemplate = resultsPartInfoTemplate.replace("{ {url} }", productUrl);
-          resultsPartInfoTr.innerHTML = resultsPartInfoTemplate;
-          resultsPartInfoEl.appendChild(resultsPartInfoTr);
-
-          resultsTable.setAttribute("border", "1");
-          resultsTable.setAttribute("cellpadding", "5");
-          resultsTable.setAttribute("cellspacing", "5");
-
-        };
-      });
-    };
+    AutoSuggest.parseResponse = submitSearch;
 
     document.querySelector(".test").appendChild(dataSrc);
-
-
   });
-
-
-//css scripts
-
-// function addBorders(){
-//   var suggestions = document.querySelector(".parts-search-suggestions > ul");
-//   console.log(suggestions.childNodes.length);
-//   if (suggestions.childNodes.length > 0) {
-//     suggestions.className = "active";
-//   } else {
-//     suggestions.className = "";
-//   } 
-// };
 
 
 
